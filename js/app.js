78fa108bbb9006c7765382bb1e45ffa2bd29b98e
Sonia Botsarova <sonia.botsarova@gmail.com>
2021-10-13 23:22:46 +0100

    create animation

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const buttonMenu = document.getElementById('button-menu');
+const buttonReserv = document.getElementById('button-reserv');  
+const body = document.getElementById('body');
+const bgMenu = document.getElementById('bg-menu');
+const bgReserv = document.getElementById('bg-reserv');
+
+buttonMenu.addEventListener('mouseenter', () => {
+    bgMenu.style.opacity = 1;
+});
+
+buttonReserv.addEventListener('mouseenter', () => {
+    bgReserv.style.opacity = 1;
+});
+
+buttonMenu.addEventListener('mouseleave', () => {
+    bgMenu.style.opacity = 0;
+});
+buttonReserv.addEventListener('mouseleave', () => {
+    bgReserv.style.opacity = 0;
+});
+
+
+
