acb97f751a880cab7f0cc0746ad13698dde430cd
sonianb <sonia.botsarova@gmail.com>
2021-12-01 16:12:58 +0000

    deploy check

--- js/reservation-form.js
+++ js/reservation-form.js
@@ -2,17 +2,20 @@
 function logSubmit(event) {
     const firstName = firstNameElem.value;
     console.log('form submitted', event);
-    log.textContent = `Form Submitted! Time stamp: ${firstName}`;
+    log.textContent = `Thank you, ${firstName}. See you soon!`;
     event.preventDefault();
   }
   
   const form = document.getElementById('form');
+  const telError = document.getElementById('telError');
   const firstNameElem = document.getElementById('firstName');
   const log = document.getElementById('log');
   form.addEventListener('submit', logSubmit);
   
 function telValidate(tel) {
   if (tel.value.length !=10) {
-    alert("Telephone numbers must be 10 digits!")
+    telError.innerHTML = "Phone number invalid";
+  } else {
+    telError.innerHTML = "";
   }
 }
